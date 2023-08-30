1d89e232e27a6fc078fa9d85aa4ce079c7f4f1cc
Pedro Miranda <pedroemanuelsm0@outlook.com>
2023-08-30 10:56:26 -0300

    Acrescentando botões de pausar e continuar, e um botão para mudar a fonte do texto

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+// lista de fontes
+const listaFontes = ["Orbitron", "Arial", "Verdana", "Open Sans" , "Monoton"];
+let indiceFontAtual = 0;
+
+const elementoTimer = document.querySelector(".timer");
+const botaoMudarFont = document.querySelector("#changeFont");
+const elementoTitulo = document.querySelector(".container h1"); // Adicionado para selecionar o título
+
+
+// Função para atualizar a fonte com base no índice atual
+function atualizarFonte(){
+
+    const fonteSelecionada = listaFontes[indiceFontAtual];
+    elementoTimer.style.fontFamily = fonteSelecionada;
+    elementoTitulo.style.fontFamily = fonteSelecionada;
+
+    // Atualize também o separador
+    const elementoSeparator = document.querySelector(".separator");
+    elementoSeparator.style.fontFamily = fonteSelecionada;
+    
+}
+
+// Atualize a fonte inicialmente com base no índice atual
+atualizarFonte();
+
+
+botaoMudarFont.addEventListener("click", function(){
+    // avança para a proxima fonte na lista
+    indiceFontAtual = ( ++indiceFontAtual ) % listaFontes.length;
+
+    atualizarFonte();
+})
+
+
 const pegaMinutes = document.querySelector("#minutes");
 const pegaSeconds = document.querySelector("#seconds");
 const pegaMilliseconds = document.querySelector("#milliseconds");
@@ -7,15 +41,20 @@ const pegaResume = document.querySelector("#resume");
 const pegaReset = document.querySelector("#reset");
 
 
-
 let interval;
 let countdownValue = 3600; // o valor inicial da contagem regressiva é definida em segundos (3600 = 1 hora)
 let isRunning = false;
+
+
+
 
 pegaStart.addEventListener("click", startCountdown);
 pegaPause.addEventListener("click", pauseCountdown);
 pegaResume.addEventListener("click", resumeCountdown);
 pegaReset.addEventListener("click", resetCountdown);
+
+
+
 
 function startCountdown(){
     if (!isRunning && countdownValue > 0){
@@ -29,6 +68,11 @@ function startCountdown(){
             }
         }, 1000); // Executa a cada 1 segundo (1000 milissegundos)
     }
+
+    // mostrar botões relevantes
+    pegaPause.style.display = "block";
+    pegaReset.style.display = "block";
+    pegaStart.style.display = "block";
 }
 
 function pauseCountdown(){
@@ -36,23 +80,17 @@ function pauseCountdown(){
         clearInterval(interval);
         isRunning = false;
     }
+
+    // Mostrar botão "resume" e ocultar botão "pause"
+    pegaResume.style.display = "block";
+    pegaPause.style.display = "block";
 }
 
 function resumeCountdown(){
     if (!isRunning && countdownValue > 0){
-        isRunning = true;
-        interval = setInterval(()=>{
-            countdownValue--;
-            updateDisplay();
-
-            if (countdownValue < 0){
-                stopCountdown();
-            }
-        }, 1000);
+        startCountdown();   // Reutiliza a função startCountdown para retomar a contagem
     }
 }
-
-
 
 
 function resetCountdown(){
@@ -60,6 +98,13 @@ function resetCountdown(){
     isRunning = false;
     countdownValue = 3600; // Redefine o valor inicial da contagem regressiva em segundos (1 hora)
     updateDisplay();
+
+
+    // Ocultar os botões "pause", "resume" e "reset" e mostra o botão "play" novamente
+    pegaPause.style.display = "none";
+    pegaResume.style.display = "none";
+    pegaReset.style.display = "none";
+    pegaStart.style.display = "block";
 }
 
 function updateDisplay(){
@@ -76,3 +121,5 @@ function formatTime( getstime )
 }
 
 updateDisplay();    // Chamada inicial para exibir o valor inicial
+
+
